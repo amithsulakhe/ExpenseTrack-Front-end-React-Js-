6763207d625d72eb56f7a9ee288d92b1a31ba965
amithsulakhe <amithsulakhe729@gmail.com>
2026-01-05 21:47:59 +0530

    Add test script in package.json and create dummyTest.js for basic test validation

---
+++ dummyTest.js
@@ -0,0 +1,14 @@
+const isTestPassed = true;
+
+if (isTestPassed) {
+    console.log("Test passed");
+    process.exit(0);    
+} else {
+    console.log("Test failed");
+    process.exit(1);
+}
+
+
+
+
+
